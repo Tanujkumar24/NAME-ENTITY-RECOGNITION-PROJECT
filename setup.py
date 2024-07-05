ab918f1a09d406e3b2eec4dd6e253a634da48e1e
Tanujkumar24 <tanuj.mangalapally@gmail.com>
2024-07-05 13:54:48 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="Tanujkumar",
+    author_email="tanuj.mangalapally@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
